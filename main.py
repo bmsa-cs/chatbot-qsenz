16c5e469e7e56a346d62fa04bf43e4a401b15ecc
Quentin Senz <qsenz24@biomedscienceacademy.org>
2021-01-20 18:20:46 +0000

    I finished the bot. I added use of the random module and added a few more questions.

--- main.py
+++ main.py
@@ -47,11 +47,33 @@ if q2 == "no" or q2 == "No":
   print("I'm sorry to hear that.")
   q3 = input("Could I make you feel better with a joke? ")
   if q3 == "yes" or "sure" or "ok":
-    print("Why did the chicken cross the road...")
-    time.sleep(4)
-    print("to get to the other side!")
-  elif q3 == "no" or q3 == "no"
-elif q2 == "stop" or q2 == "stop talking" or q2 == "stop talking to me":
-  print("Alright then, chatbot Bristol powering off...")
+    one = "Why did the chicken cross the road? To get to the other side!"
+    two = "I invented a new word... Plagarism!"
+    three = "Hear about the new resturant called karma? Heres the menu: You get what you deserve."
+    joke = random.choice([one, two, three])
+    print(joke)
+  elif q3 == "no" or q3 == "nope" or q3 == "No":
+    print("Alright then i'll go.")
+    exit()
+  else:
+    print("I'm not sure how to respond to that...")
+    exit()
+
+elif q2 == "yes" or q2 == "yeah" or q2 == "Yes":
+  print("That's very nice to hear!")
+  q4 = input("Would you like to hear a joke?")
+  if q4 == "yes" or q4 == "Yes" or q4 == "sure":
+    one = "Why did the chicken cross the road? To get to the other side!"
+    two = "I invented a new word... Plagarism!"
+    three = "Hear about the new resturant called karma? Heres the menu: You get what you deserve."
+    joke = random.choice([one, two, three])
+    print(joke)
 else:
   print("Sorry, im not sure how to respond to this.")
+  exit()
+
+q5 = input("What's your favorite food?")
+print("Awesome my favorite food is stir fry!")
+
+q6 = input("How was your conversation with me 1-5?")
+print("Thanks for your feedback", q1)
